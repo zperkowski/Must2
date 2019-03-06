d884aeb9328c36a7efd4397863486bba5439d471
Zdzisław Perkowski <ZdzislawPerkowski@outlook.com>
2019-03-06 20:11:14 +0100

    UI

---
+++ InstrumentCatalog/ViewModel/InstrumentListViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstrumentCatalog.ViewModel
+{
+    public class InstrumentListViewModel : INotifyPropertyChanged
+    {
+        private BLC.BLC blc;
+
+        private ObservableCollection<IInstrument> instruments;
+
+        public ObservableCollection<IInstrument> Instruments
+        {
+            get => instruments;
+            set
+            {
+                instruments = value;
+                OnPropertyChanged(nameof(Instruments));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private ObservableCollection<IProducer> producers;
+
+        public ObservableCollection<IProducer> Producers
+        {
+            get => producers;
+        }
+    }
+}
---
+++ InstrumentCatalog/ViewModel/InstrumentViewModel.cs
@@ -0,0 +1,87 @@
+﻿
+using Core;
+using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstrumentCatalog.ViewModel
+{
+    public class InstrumentViewModel : INotifyPropertyChanged, IInstrument
+    {
+
+        public InstrumentViewModel(IInstrument instrument)
+        {
+            this.instrument = instrument;
+        }
+
+        private IInstrument instrument;
+
+        public int ID
+        {
+            get => instrument.ID;
+            set
+            {
+                instrument.ID = value;
+                OnPropertyChanged(nameof(ID));
+            }
+        }
+
+        public string Name
+        {
+            get => instrument.Name;
+            set
+            {
+                instrument.Name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public int ProductionYear
+        {
+            get => instrument.ProductionYear;
+            set
+            {
+                instrument.ProductionYear = value;
+                OnPropertyChanged(nameof(ProductionYear));
+            }
+        }
+
+        public IProducer Producer
+        {
+            get => instrument.Producer;
+            set
+            {
+                instrument.Producer = value;
+                OnPropertyChanged(nameof(Producer));
+            }
+        }
+
+        public Core.InstrumentType Transmission
+        {
+            get => instrument.Type;
+            set
+            {
+                instrument.Type = value;
+                OnPropertyChanged(nameof(Transmission));
+            }
+        }
+
+        public InstrumentType Type { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        //INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
